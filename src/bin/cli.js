711b46a001070cfbc4606c0987b46c2b50594ddc
Toorusr <toorusr@protonmail.com>
2018-06-07 20:21:42 +0200

    Add: meow cli handling

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,12 +1,38 @@
 const dir = require("../lib/directory")
 const fs = require('fs');
+const meow = require('meow');
+
 // use cli meow!
 
-if (process.argv.length <= 2) {
+/*if (process.argv.length <= 2) {
     console.log("\nUsage: " + __filename + " <root-dir-path>\n");
     process.exit(-1);
+} */
+
+const cli = meow(`
+    Usage
+      $ sitemap-lcl <dir-path>
+
+    Options
+      --scan, -s Scan for
+
+    Example
+      $ sitemap-lcl -s ~/website
+      Fetching sitemap! [===<=>===]
+    `,{
+        flags: {
+            test: {
+                type: 'boolean',
+                alias: 't'
+            }
+        }
+    }
+);
+
+
+if ( process.argv.length <= 2 ) {
+  console.log( "SITEMAP-LCL \n Specify `--help` for available options." );
+  process.exit(-1);
 }
 
-
-
-let tree = dir.echo(process.argv[2]);
+const tree = dir.tree(cli.input[0]);
